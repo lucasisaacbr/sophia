6807fff94a98f6786398618dcc1d559b4c33b293
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-03-06 20:14:20 -0300

    Create login front-end and serve an route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,10 @@
 		app.get("/", function (req, res) {
 			res.status(200).send("OK");
 		});
+
+		app.get("/login", function (req, res) {
+			res.status(200).render("./login_module/index.html");
+		});
 	}
 
 }());
