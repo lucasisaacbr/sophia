b5e32e778e879a1dddadfcff236028bfc3fbe5c5
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-03-06 20:48:59 -0300

    create login-Form component

---
+++ client/login_module/js/components/loginForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p> {{ email }} </p>
+        <input type="text" />
+        <input type="text" />
+        <div>
+            <button>Confirm</button>
+            <button>Register</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    (function () {
+        "use strict";
+
+        module.exports = {
+
+        }
+
+	}());
+</script>
