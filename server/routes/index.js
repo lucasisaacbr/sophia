adcc0139c409f076d1b568b574e2cb4e278fbf07
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-03-08 08:43:42 -0300

    Add dependencies to router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,21 @@
 (function () {
 	"use strict";
 
-	module.exports = function (app) {
+	const ensureAuthenticated = function (req, res, next) {
+		return req.isAuthenticated() || process.isLocal ? next() : res.status(403).render("./login_module/index.html", {"reason": "forbidden"});
+	};
 
-		app.get("/", function (req, res) {
-			res.status(200).send("OK");
-		});
+	module.exports = function (app, conversationFactory, cloudantFactory, passport, conversationMethods) {
+
+		require("./helpers/loginHelper")(app, passport);
 
 		app.get("/login", function (req, res) {
 			res.status(200).render("./login_module/index.html");
 		});
+
+		app.get("/teste", ensureAuthenticated, function (req, res) {
+			res.status(200).send("Logado");
+		})
 	}
 
 }());
