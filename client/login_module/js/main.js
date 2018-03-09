8ca9ba5cbbf29f235060c8e1b32fe737b67861c8
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-03-09 19:12:56 -0300

    Add vuex instantiation to main.js orchestrator

--- client/login_module/js/main.js
+++ client/login_module/js/main.js
@@ -2,10 +2,15 @@
 	"use strict";
 
 	const Vue = require("vue");
+	const Vuex = require("vuex");
+
+	Vue.use(Vuex);
 
 	return new Vue({
 		"el": "#app",
 		"name": "LoginModule",
+		"store": require("./store/store"),
+
 		"render": function (h) {
 			return h(require("./components/app.vue"));
 		}
