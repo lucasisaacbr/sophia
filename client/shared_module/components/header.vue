40056025d1514771ef42ceacd69d4bc5a86bed70
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-03-09 19:13:18 -0300

    Create header shared component

---
+++ client/shared_module/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app-header">
+
+    </div>
+</template>
+
+<script>
+    (function () {
+        "use strict";
+
+        module.exports = {
+
+
+
+        }
+
+	}());
+</script>
+
+<style>
+
+</style>
