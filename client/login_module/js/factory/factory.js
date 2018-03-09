f668bf5ba9c26ecb96bfe487c89662d1bde3bbec
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-03-09 19:52:49 -0300

    Create a factory to orchestrate xhttp requests

---
+++ client/login_module/js/factory/factory.js
@@ -0,0 +1,14 @@
+(function () {
+	"use strict";
+
+	const httpClient = require("../../../shared_module/js/http");
+
+	module.exports = {
+
+		"getActiveUser": function () {
+			return httpClient.get("/userInfo");
+		}
+
+	}
+
+});
