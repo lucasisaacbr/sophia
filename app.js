9ca791c4e57fc6a5fb623a6cac2a8ab9c40f25de
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-03-29 18:58:00 -0300

    Implement secure SSL

--- app.js
+++ app.js
@@ -1,68 +1,85 @@
 (function () {
 	"use strict";
+	require("dotenv").config({
+		"silent": true
+	});
 
-	const dotenv = require("dotenv");
-	dotenv.load();
-	dotenv.config({"silent": true});
-
+	const appPort = process.env.APP_PORT || process.env.PORT || process.env.VCAP_APP_PORT || 6050;
+	const fs = require("fs");
 	const express = require("express");
-	const bodyParser = require("body-parser");
-	const engines = require("consolidate");
-
-	const port = process.env.PORT || 3001;
-	const path = require("path");
+	const helmet = require("helmet");
+	const app = express();
+	let server;
+	if (process.env.LOCAL_HTTPS) {
+		server = require("https").createServer({
+			"hostname": "localhost",
+			"agent": false,
+			"key": fs.readFileSync("./root/certificates/local/localhost-privkey.pem"),
+			"cert": fs.readFileSync("./root/certificates/local/localhost-cert.pem"),
+			"rejectUnauthorized": false
+		}, app);
+	} else {
+		server = require("http").createServer(app);
+	}
 	const cookieSession = require("cookie-session");
 	const cookieParser = require("cookie-parser");
+	const passport = require("passport");
+	const compress = require("compression");
+	const engines = require("consolidate");
+	const morgan = require("morgan");
 
-	// Cloudant
-	const cloudantConfig = require("./server/configs/cloudant").init;
-	const cloudantFactory = require("./server/helpers/cloudant")(cloudantConfig);
+
+	// MongoDB
+	const mongoDB = new require("./server/helpers/mongo");
+	const userModel = require("./server/helpers/userModel")(mongoDB);
+
 
 	// Conversation
-	const conversationConfig = require("./server/configs/conversation");
-	const conversationMethods = require("./server/helpers/conversation")(cloudantFactory);
-	const conversationFactory = require("./server/helpers/conversationFactory")(conversationConfig, conversationMethods);
+	// const conversationConfig = require("./server/configs/conversation");
 
-	// Login dependencies
-	const passport = require("passport");
-	const passportFactory = require("./server/helpers/passport")(passport, cloudantFactory);
-
-	const app = express();
-
-	app.use(bodyParser.urlencoded({extended: true}));
-	app.use(bodyParser.json());
+	app.use(helmet());
+	app.use(compress());
 	app.use(cookieParser());
 	app.use(cookieSession({
-		"secret": process.env.SECRET_KEY,
-		"maxAge": 8640000
+		"secret": process.env.APP_SECRET,
+		"maxAge": 86400000,
+		"saveUninitialized": false,
+		"resave": false,
+		"name": "sophia",
+		"key": "sophiakey",
+		"cookie": {
+			"secure": true,
+			"httpOnly": true
+		}
 	}));
-
-	app.use(passport.initialize());
-	app.use(passport.session());
-
-	app.use(express.static(path.join(__dirname + '/client')));
 
 	app.engine("html", engines.ejs);
 	app.set("view engine", "ejs");
 	app.set("views", __dirname + "/client");
-	app.disable("x-powered-by");
+	app.use(express.static(__dirname + "/client"));
+	app.use(express.json());
+	app.use(express.urlencoded({
+		"extended": true,
+		"limit": "10mb"
+	}));
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-	app.all("/*", function (req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-		res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-type, Accept, X-Access-Token, X-Key");
 
-		if (req.method === "OPTIONS") {
-			res.status(200).end();
-		} else {
-			next();
-		}
-	});
+	if (process.env.DEBUG) {
+		app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
+	}
 
-	require("./server/routes/index")(app, conversationFactory, cloudantFactory, passport, conversationMethods);
+	server.listen(appPort, function () {
+		console.log("Server is running on port " + appPort);
+		mongoDB.connect().then(() => {
+			console.log("MongoDB connected");
+			require("./server/helpers/passport")(passport, userModel);
+			require("./server/routes/index")(app, passport);
+		});
 
-	app.listen(port, function () {
-		console.log("Server is running on port " + port);
+
 	});
 
 }());
+
