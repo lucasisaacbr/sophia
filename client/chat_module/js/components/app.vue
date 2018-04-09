2e6f7d15abba6560ab993042325d234027b71f02
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-04-09 19:48:22 -0300

    Create app.vue front-end and some logic

--- client/chat_module/js/components/app.vue
+++ client/chat_module/js/components/app.vue
@@ -1,3 +1,87 @@
-(function () {
-	"use strict";
-}());
+<template>
+    <div id="app">
+        <header class="sh-head">
+
+        </header>
+        <div class="sh-messages">
+
+        </div>
+        <footer class="sh-chat">
+            <div class="field has-addons msg-input">
+                <div class="control input-container">
+                    <input class="input" type="text" placeholder="Digite sua menssagem.." v-model="message">
+                </div>
+                <div class="control">
+                    <a class="button is-info" @click="send" @keyup.enter="send">
+                        Enviar
+                    </a>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+</template>
+
+<script>
+	(function () {
+		"use strict";
+
+		let factory = require("../factory/factory");
+
+		module.exports = {
+			"data": function () {
+				return {
+					"message": ""
+				}
+			},
+			"methods": {
+				"send": () => {
+					factory.sendMessage(this.message)
+						.then(response => {
+                            // Treat chat response
+						})
+                        .catch(err => console.error(err));
+				}
+			}
+		}
+
+	}());
+</script>
+
+<style>
+
+    #app {
+        flex-direction: column;
+    }
+
+    .sh-head {
+        height: 30px;
+        width: 100%;
+        padding: 5px 8px;
+        background-color: darkslateblue
+    }
+
+    .sh-chat {
+        height: 65px;
+        background-color: silver;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .sh-messages {
+        background-color: #f5f5f5;
+        height: 100%;
+    }
+
+    .msg-input {
+        width: 100%;
+        padding: 0 25px;
+    }
+
+    .input-container {
+        width: 100%;
+    }
+
+
+</style>
