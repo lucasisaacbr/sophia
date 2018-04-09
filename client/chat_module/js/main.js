c972a1c228208c5f5a9e262cf423f4be557e71ba
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-04-09 19:47:43 -0300

    Config Vue instance

--- client/chat_module/js/main.js
+++ client/chat_module/js/main.js
@@ -1,3 +1,19 @@
 (function () {
 	"use strict";
+
+	const Vue = require("vue");
+	const Vuex = require("vuex");
+
+	Vue.use(Vuex);
+
+	return new Vue({
+		"el": "#app",
+		"name": "ChatModule",
+		"store": require("./store/store"),
+
+		"render": function (h) {
+			return h(require("./components/app.vue"));
+		}
+	});
+
 }());
