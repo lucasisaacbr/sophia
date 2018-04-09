348e63c79aa7914bae2ce5f9b681cf000095ae74
Lucas Isaac A G Mathias <lisaac@br.ibm.com>
2018-04-09 19:48:05 -0300

    Create sendMessage factory method

--- client/chat_module/js/factory/factory.js
+++ client/chat_module/js/factory/factory.js
@@ -1,12 +1,32 @@
 (function () {
 	"use strict";
 
-	const httpClient = require("../../../shared_module/js/http");
+	const httpClient = require("../../../etc/js/http");
 
 	module.exports = {
-
 		"getActiveUser": function () {
 			return httpClient.get("/userInfo");
+		},
+		"login": function (username, password) {
+			return new Promise((resolve, reject) => {
+				httpClient.post("/login", {"username": username, "password": password})
+					.then(function (data) {
+						return resolve(data);
+					}).catch(err => {
+					return reject(err);
+				});
+			})
+
+		},
+		"sendMessage": function (message) {
+			return new Promise((resolve, reject) => {
+				httpClient.post("/sendMessage", {"message": message})
+					.then(function (data) {
+						return resolve(data);
+					}).catch(err => {
+					return reject(err)
+				})
+			});
 		}
 	}
 
