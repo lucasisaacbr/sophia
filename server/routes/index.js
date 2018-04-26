ee170f73ba436771e1c72aa5369f34b30bdbede2
lisaac <lisaac@br.ibm.com>
2018-04-26 08:27:35 -0300

    Include feedackModel to their specific route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,11 @@
 	};
 
 
-	module.exports = function (app, passport, watsonAssistant) {
+	module.exports = function (app, passport, watsonAssistant, feedbackModel) {
 
 		require("./helpers/loginHelper")(app, passport);
 		require("./helpers/chatHelper")(app, ensureAuthenticated, watsonAssistant);
+		require("./helpers/feedbackHelper")(app, feedbackModel);
 
 		app.get("/teste", ensureAuthenticated, function (req, res) {
 			res.status(200).send("Logado");
