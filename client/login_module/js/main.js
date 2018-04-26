f64614610f4e7c8290e06ed22e3791bed7f12a2b
lisaac <lisaac@br.ibm.com>
2018-04-26 19:02:26 -0300

    remove store dependencies

--- client/login_module/js/main.js
+++ client/login_module/js/main.js
@@ -2,15 +2,10 @@
 	"use strict";
 
 	const Vue = require("vue");
-	const Vuex = require("vuex");
-
-	Vue.use(Vuex);
 
 	return new Vue({
 		"el": "#app",
 		"name": "LoginModule",
-		"store": require("./store/store"),
-
 		"render": function (h) {
 			return h(require("./components/app.vue"));
 		}
