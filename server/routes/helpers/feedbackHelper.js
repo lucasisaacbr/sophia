95a567bdf8d47463a1b2faf83b51ed6845399a3d
lisaac <lisaac@br.ibm.com>
2018-04-26 08:27:55 -0300

    Create a feedbackHelper routes

---
+++ server/routes/helpers/feedbackHelper.js
@@ -0,0 +1,26 @@
+(function () {
+	"use strict";
+
+	 module.exports = function (app, feedbackModel) {
+
+	 	app.post("/insertFeedback", (req, res) => {
+		 	let feedbackObject = req.body.lastResponseObject;
+
+		 	feedbackModel.insertFeedback(feedbackObject)
+				.then((mongoResponse) => {
+					console.log(mongoResponse);
+					res.status(200).send(mongoResponse);
+				})
+				.catch(mongoError => res.status(500).send(mongoError));
+		 });
+
+		 app.post("/getAllFeedbacks", (req, res) => {
+		 	feedbackModel.getAll()
+				.then((mongoResponse) => {
+					res.status(200).send(mongoResponse);
+				}).catch(mongoError => res.status(500).send(mongoError));
+
+		 });
+	 }
+
+}());
