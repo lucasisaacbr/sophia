4cf58314155a9725346c2d9a17307401a23b4fee
lisaac <lisaac@br.ibm.com>
2018-04-30 16:30:13 -0300

    Create Side Menu for dashboard

---
+++ client/dashboard_module/js/components/sideOptions.vue
@@ -0,0 +1,65 @@
+<template>
+    <aside class="menu sh-menu">
+        <p class="menu-label">
+            Feedbacks
+        </p>
+        <ul class="menu-list">
+            <li><a class="is-active">Positivos</a></li>
+            <li><a>Negativos</a></li>
+        </ul>
+        <p class="menu-label">
+            Intenções
+        </p>
+        <ul class="menu-list">
+            <li><a>Listar Intenções</a></li>
+            <li><a>Adicionar Exemplos</a></li>
+            <li><a>Adicionar Contra-exemplos</a></li>
+        </ul>
+        <p class="menu-label">
+            Entidades
+        </p>
+        <ul class="menu-list">
+            <li><a>Listar Entidades</a></li>
+            <li><a>Adicionar Valores</a></li>
+            <li><a>Adicionar Synonimos</a></li>
+        </ul>
+        <p class="menu-label">
+            Metricas
+        </p>
+        <ul class="menu-list">
+            <li><a>Dashboard de Métricas</a></li>
+
+        </ul>
+    </aside>
+</template>
+
+<script>
+(function () {
+    "use strict";
+    module.exports = {
+        "props": [],
+        "name": "SideOptions",
+        "components": {
+        },
+        "data": function () {
+            return {
+
+            }
+        },
+        "methods": {
+
+        }
+    };
+
+}());
+</script>
+
+<style>
+    .sh-menu {
+        max-width: 25%;
+        min-width: 210px;
+        flex-grow: 1;
+        background: papayawhip;
+        padding: 7px;
+    }
+</style>
