059d9eddd7ab5e39549bbfd9b48b9adaa20bb1fc
lisaac <lisaac@br.ibm.com>
2018-04-30 16:29:58 -0300

    Create dashboard UI

--- client/dashboard_module/js/components/app.vue
+++ client/dashboard_module/js/components/app.vue
@@ -1,3 +1,50 @@
+<template>
+	<div id="app">
+        <header class="sh-head"></header>
+        <div class="sh-container">
+            <side-options></side-options>
+        </div>
+    </div>
+</template>
+
+<script>
 (function () {
-	"use strict";
+    "use strict";
+    module.exports = {
+        "props": [],
+        "name": "DashbordModule",
+        "components": {
+            "sideOptions": require("./sideOptions.vue")
+        },
+        "data": function () {
+            return {
+
+            }
+        },
+        "methods": {
+
+        }
+    };
+
 }());
+</script>
+
+<style>
+    #app {
+        flex-direction: column;
+    }
+
+    .sh-head {
+        height: 30px;
+        width: 100%;
+        padding: 5px 8px;
+        background-color: darkslateblue
+    }
+
+    .sh-container {
+        display: flex;
+        flex-direction: row;
+        height: 100%;
+        width: 100%;
+    }
+</style>
