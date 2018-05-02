91f8023725481bd48dfbf1b165c5de5f274e39a0
lisaac <lisaac@br.ibm.com>
2018-05-02 19:34:15 -0300

    Implement Vuex

--- client/dashboard_module/js/main.js
+++ client/dashboard_module/js/main.js
@@ -2,14 +2,17 @@
 	"use strict";
 
 	const Vue = require("vue");
+	const Vuex = require("vuex");
 	require("@fortawesome/vue-fontawesome");
 	const vueFontawesome = require("@fortawesome/vue-fontawesome").FontAwesomeIcon;
 
+	Vue.use(Vuex);
 	Vue.component("FontAwesomeIcon", vueFontawesome);
 
 	return new Vue({
 		"el": "#app",
 		"name": "DashboardModule",
+		"store": require("./store/store"),
 		"render": function (h) {
 			return h(require("./components/app.vue"));
 		}
