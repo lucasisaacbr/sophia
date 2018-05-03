26828d10508dc4f33a69b3c9ed6f8b7cd3ab506b
lisaac <lisaac@br.ibm.com>
2018-05-03 08:45:58 -0300

    Add the workspace_id as a parameter

--- server/routes/helpers/watsonHelper.js
+++ server/routes/helpers/watsonHelper.js
@@ -3,19 +3,23 @@
 
 	let credentials = {
 		"username": process.env.ASSISTANT_USER,
-		"password": process.env.ASSISTANT_PASSWORD
+		"password": process.env.ASSISTANT_PASSWORD,
+	};
+
+	let workspace_id = {
+		"workspace_id": process.env.ASSISTANT_WORKSPACE
 	};
 
 	module.exports = function (app, watsonAssistant) {
 
 		app.post("/getIntents", (req, res) => {
-			watsonAssistant.getIntent(credentials)
+			watsonAssistant.listIntents(credentials, workspace_id)
 				.then((intents) => res.status(200).send(intents))
 				.catch(err => res.status(500).send(err));
 		});
 
 		app.post("/getEntities", (req, res) => {
-			watsonAssistant.getEntity(credentials)
+			watsonAssistant.listEntities(credentials, workspace_id)
 				.then((intents) => res.status(200).send(intents))
 				.catch(err => res.status(500).send(err));
 		});
