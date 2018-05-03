834946e02e878733a7f06dfef9fc9c7fd98b77f8
lisaac <lisaac@br.ibm.com>
2018-05-03 08:30:55 -0300

    Create actions to control state of entities and intents.

--- client/dashboard_module/js/store/feedbacks/actions.js
+++ client/dashboard_module/js/store/feedbacks/actions.js
@@ -3,6 +3,9 @@
 
 	const getPositiveFB = require("../../factory/factory").getPositivesFB;
 	const getNegativeFB = require("../../factory/factory").getNegativesFB;
+	const getEntities = require("../../factory/factory").getEntities;
+	const getIntents = require("../../factory/factory").getIntents;
+
 
 	module.exports = {
 
@@ -17,6 +20,16 @@
 			getNegativeFB().then((fb => {
 				context.commit("negativeFB", fb);
 			}).catch(err => console.error(err)));
+		},
+		"entities": function (context) {
+			getEntities().then(entities => {
+				context.commit("entities", entities);
+			}).catch(err => console.error(err));
+		},
+		"intents": function (context) {
+			getIntents().then(intents => {
+				context.commit("intents", intents);
+			}).catch(err => console.error(err));
 		}
 
 	};
