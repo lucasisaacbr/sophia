2e0f1f56fdb006df679b76244b5c464a24bc59e2
lisaac <lisaac@br.ibm.com>
2018-05-07 08:20:48 -0300

    Create main content area

---
+++ client/dashboard_module/js/components/mainContent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="sh-content">
+       <negative-content v-if="getView === 'negative'"></negative-content>
+    </div>
+</template>
+
+<script>
+(function () {
+    "use strict";
+
+    module.exports = {
+        "props": [],
+        "name": "MainContent",
+        "components": {
+			"negativeContent": require("./negative-content.vue")
+		},
+        "data": function () {
+            return {
+
+            }
+        },
+        "methods": {
+
+        },
+        "computed": {
+            getView() {
+				return this.$store.getters["feedbacks/getView"];
+            }
+        },
+        "beforeCreate": function () {
+			this.$store.dispatch("feedbacks/negativeFB");
+		}
+    }
+
+}());
+</script>
+
+<style>
+    .sh-content{
+        padding: 7px;
+        background-color: #f5f5f5;
+        width: 100%;
+    }
+
+    .fb-row {
+
+    }
+</style>
