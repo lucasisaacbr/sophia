688387b493282fc99397638e62add5b889d1f5f4
lisaac <lisaac@br.ibm.com>
2018-05-07 08:21:13 -0300

    View for negatives feedbacks

---
+++ client/dashboard_module/js/components/negative-content.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h2>Feedbacks Negativos</h2>
+        <div>
+            <ul>
+                <li v-for="feedback in getNegatives" class="fb-row"> {{ feedback }} </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+(function () {
+    "use strict";
+
+    module.exports = {
+        "props": [],
+        "name": "NegativeContent",
+        "data": function () {
+            return {
+
+            }
+        },
+        "methods": {
+
+        },
+        "computed": {
+			getNegatives() {
+				return this.$store.getters["feedbacks/getNegatives"];
+			}
+        },
+        "beforeCreate": function () {
+            this.$store.dispatch("feedbacks/negativeFB");
+        }
+    }
+
+}());
+</script>
+
+<style>
+
+</style>
