45e89303c9b22c341cbfcc8f07b7f21a07d4c699
lisaac <lisaac@br.ibm.com>
2018-05-10 18:34:12 -0300

    Create entities view

---
+++ client/dashboard_module/js/components/entities-content.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="sh-content-container">
+        <h2>Entidades</h2>
+        <div> {{ getEntities }}</div>
+    </div>
+</template>
+
+<script>
+	(function () {
+		"use strict";
+
+		module.exports = {
+			"props": [],
+			"name": "EntitiesContent",
+			"data": function () {
+				return {}
+			},
+			"methods": {
+
+			},
+			"computed": {
+				getEntities() {
+					return this.$store.getters["feedbacks/getEntities"];
+				}
+			},
+			"beforeCreate": function () {
+				this.$store.dispatch("feedbacks/getEntities");
+			}
+		}
+
+	}());
+</script>
+
+<style>
+
+</style>
