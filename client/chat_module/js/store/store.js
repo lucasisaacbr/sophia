3e55af33efe1b8e37689a0db8d016f0341d4b473
lisaac <lisaac@br.ibm.com>
2018-05-10 19:03:58 -0300

    Add store JS

---
+++ client/chat_module/js/store/store.js
@@ -0,0 +1,13 @@
+(function () {
+	"use strict";
+
+	const Vuex = require("vuex");
+
+	module.exports = new Vuex.Store({
+		"modules": {
+			"user": require("./user/module")
+		},
+		"strict": process.env.NODE_ENV !== "production"
+	});
+
+}());
