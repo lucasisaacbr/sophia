484dea3f54c7b0a1f09b017558dd7e62230d35d8
lisaac <lisaac@br.ibm.com>
2018-05-10 18:34:23 -0300

    Create intents view

---
+++ client/dashboard_module/js/components/intents-content.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="sh-content-container">
+        <h2>Intenções</h2>
+        <div> {{ getIntents }}</div>
+    </div>
+</template>
+
+<script>
+	(function () {
+		"use strict";
+
+		module.exports = {
+			"props": [],
+			"name": "IntentsContent",
+			"data": function () {
+				return {}
+			},
+			"methods": {
+
+			},
+			"computed": {
+				getIntents() {
+					return this.$store.getters["feedbacks/getIntents"];
+				}
+			},
+			"beforeCreate": function () {
+				this.$store.dispatch("feedbacks/getIntents");
+			}
+		}
+
+	}());
+</script>
+
+<style>
+
+</style>
