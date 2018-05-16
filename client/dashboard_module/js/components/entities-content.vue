b31be1877ab5f8b321d2a887c22f1c444c5f6aab
lisaac <lisaac@br.ibm.com>
2018-05-16 17:15:10 -0300

    Create entities content

--- client/dashboard_module/js/components/entities-content.vue
+++ client/dashboard_module/js/components/entities-content.vue
@@ -11,12 +11,38 @@
                 <div class="intent-card-content">
                     <div class="intent-name">
                         <h1><span class="intent-hash">@</span>{{ entity.entity }}
-                            <span class="intent-delete" @click="callDelete(entity.entity)">
+                            <span class="intent-delete">
+                                   <a class="button is-danger is-small" @click="callDelete(entity.entity)">
                                  <font-awesome-icon
                                          :icon="trash">
                                 </font-awesome-icon>
+                            </a>
+                            <a class="button is-primary is-small" @click="callValue(entity.entity)">
+                                 <font-awesome-icon
+                                         :icon="plusIcon">
+                                 </font-awesome-icon> <span>{{" Novo Valor"}}</span>
+                            </a>
                             </span>
                         </h1>
+                    </div>
+                    <hr/>
+                    <div class="intent-examples">
+                        <h2 class="intent-title"> Valor </h2>
+                        <div class="values-container">
+                            <div v-for="value in entity.values">
+                                <div>
+                                    <span class="synonyms">{{ value.value }}</span>
+                                </div>
+                                <h2 class="intent-title"> Sinônimos </h2>
+                                <div id="syn-container" v-if="value.synonyms.length > 0">
+                                    <div  v-for="synonym in value.synonyms">
+                                        <span class="synonyms"> {{synonym}} </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr/>
+
                     </div>
                 </div>
             </div>
@@ -40,13 +66,62 @@
             </div>
         </div>
 
+        <div class="modal" :class="[{'is-active': addValueModal}]">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Adicionar Valor e Sinônimos</p>
+                    <button class="delete" aria-label="close" @click="addValueModal = false"></button>
+                </header>
+                <section class="modal-card-body intent-name">
+                    <div>
+                        <h1>Dica: O Valor é a representação de uma entidade </h1>
+                        <h1>Ex: A entidade @pizza pode ter valores como calabresa e peperoni. </h1>
+                        <div class="field is-horizontal">
+                            <div class="field is-horizontal">
+                                <div class="field-label is-normal">
+                                    <label class="label">Valor</label>
+                                    <input class="input" type="text" v-model="newValue" placeholder="Valor">
+                                </div>
+                                <div class="field is-horizontal">
+                                    <div class="field-label is-normal">
+                                        <label class="label">Sinônimos</label>
+                                        <div class="entity-menu">
+                                            <input class="input" type="text" placeholder="Sinonimos" v-model="synonym">
+                                            <a class="button is-primary" @click="arrayOfSynonyms()">
+                                                Adicionar outro sinônimo
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="entity-values-list" v-if="newSynonyms.length > 0">
+                                <div class="field is-grouped is-grouped-multiline">
+                                    <div class="control" v-for="syn in newSynonyms">
+                                        <div class="tags has-addons">
+                                            <span class="tag is-link">{{syn}}</span>
+                                            <a class="tag is-delete" @click="popSynonym(syn)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button is-danger" @click="">Cofirmar</button>
+                    <button class="button" @click="addValueModal = false">Cancelar</button>
+                </footer>
+            </div>
+        </div>
+
 
         <div class="modal" :class="[{'is-active': createEntityModal}]">
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
                     <p class="modal-card-title">Adicionar Entidade</p>
-                    <button class="delete" aria-label="close" @click="createEntityModal = false"></button>
+                    <button class="delete" aria-label="close" @click="closeEntityModal()"></button>
                 </header>
                 <section class="modal-card-body intent-name">
                     <div>
@@ -85,7 +160,7 @@
                 </section>
                 <footer class="modal-card-foot">
                     <button class="button is-primary" :disabled='entityToCreate === ""' @click="createEntity(entityToCreate, values)">Cofirmar</button>
-                    <button class="button" @click="createEntityModal = false">Cancelar</button>
+                    <button class="button" @click="closeEntityModal()">Cancelar</button>
                 </footer>
             </div>
         </div>
@@ -106,10 +181,14 @@
 				return {
 					"showModal": false,
                     "createEntityModal": false,
+                    "addValueModal": false,
                     "entityToDelete": "",
                     "entityToCreate": "",
                     "entityValue": "",
-                    "values": []
+                    "values": [],
+                    "synonym": "",
+                    "newSynonyms": [],
+                    "newValue": ""
                 }
 			},
 			"methods": {
@@ -117,6 +196,10 @@
 					this.showModal = true;
 					this.entityToDelete = entity;
 				},
+                closeEntityModal() {
+					this.createEntityModal = false;
+					this.values = [];
+                },
                 arrayOfValues() {
 					if ( this.entityValue ) {
 						this.values.push({
@@ -125,11 +208,23 @@
 						this.entityValue = "";
                     }
                 },
+                arrayOfSynonyms() {
+					if ( this.synonym ) {
+						this.newSynonyms.push(this.synonym);
+                    }
+                    this.synonym = "";
+                },
                 popValue(value) {
 					let index = this.values.indexOf(value);
 					if (index > -1) {
 						this.values.splice(index, 1);
 					}
+                },
+                popSynonym(syn) {
+					let index = this.newSynonyms.indexOf(syn);
+					if (index > -1) {
+						this.newSynonyms.splice(index, 1);
+                    }
                 },
 				deleteEntity(entityName) {
 					factory.deleteEntity(entityName)
@@ -143,10 +238,23 @@
 					factory.createEntity(entityName, entityValues)
 						.then(() => {
 							this.createEntityModal = false;
+							this.values = [];
 							this.$store.dispatch("feedbacks/entities");
 						})
 						.catch(err => console.log(err));
-				}
+				},
+                callValue() {
+					this.addValueModal = true;
+                },
+                addValue(entity) {
+                    factory.createValue(entity, this.newValue)
+                        .then(() => {
+                        	this.addValueModal = false;
+                        	this.newValue = "";
+                        	this.$store.dispatch("feedbacks/entities");
+                        })
+                        .catch(err => console.error(err));
+                }
 			},
 			"computed": {
 				getEntities() {
@@ -170,5 +278,21 @@
 <style>
     .entity-menu {
         display: flex;
+    }
+
+    .values-container {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #syn-container {
+        display: flex;
+    }
+    .synonyms {
+        padding: 7px;
+    }
+
+    .add-value {
+
     }
 </style>
